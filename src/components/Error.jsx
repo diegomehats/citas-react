5193d2fc2061db4d8e7ad15d2ec187d34ba8c52f
Diego Mehats <diegomehats@gmail.com>
2023-09-07 17:12:56 -0300

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+
+
+const Error = ({mensaje}) => {
+  return (
+
+    <div className="bg-red-700 text-white uppercase font-bold p-3 text-center mb-3 rounded-mb">
+    <p> {mensaje} </p>
+</div>
+  )
+}
+
+export default Error
